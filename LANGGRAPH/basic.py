306b4a3b25003611645763f5db2760fb5e253b26
Vedant021004 <vedantkp79@gmail.com>
2026-08-07 02:22:11 +0530

    basic

--- LANGGRAPH/basic.py
+++ LANGGRAPH/basic.py
@@ -15,3 +15,63 @@ s1 = Classic()
 s1.sam()
 s1.sum()
 
+class Student:
+    pass
+
+s1 = Student()
+s1.age = 21
+s1.name = "vedant kapil"
+s1.job = "in malad neso soft"
+
+print(s1.age)
+print(s1.name)
+print(s1.job)
+
+class Student:
+    def obj(self,name,age):
+        self.name = name
+        self.age = age
+        
+        print(name,age)
+    
+s1 = Student()    
+s1.obj("vedant",21)
+
+
+class Car:
+    def BMW(self,price, brand ,colour):
+        self.name = price,
+        self.brand = brand,
+        self.colour = colour
+        print("heyyyy",price,brand,colour)
+
+    def AUDI(self,price, brand ,colour):
+        self.name = price,
+        self.brand = brand,
+        self.colour = colour
+        print("heyyyy",price,brand,colour)
+
+
+    def MERCIDES(self,price, brand ,colour):
+        self.name = price,
+        self.brand = brand,
+        self.colour = colour                
+        print("heyyyy",price,brand,colour)
+
+
+s1 = Car()
+s1.BMW(100000,"BMW","BLACK")
+s1.AUDI(20000,"AUDI","NEO")
+s1.MERCIDES(432940,"MERCIDES","WHITE")
+
+
+class Student:
+    pass
+
+print(Student)
+print(type(Student))
+
+x = Student
+
+print(x)
+print(type(x))        
---
+++ LANGGRAPH/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+class Student:
+    pass
+
+print(Student)
+print(type(Student))
+
+x = Student
+
+print(x)
+print(type(x))        
