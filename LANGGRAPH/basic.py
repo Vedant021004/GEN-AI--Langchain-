91ec7246baed59783eccc0987922b050b6a9849b
Vedant021004 <vedantkp79@gmail.com>
2026-08-06 21:12:16 +0530

    covering baisc required for the langgraph:

---
+++ LANGGRAPH/basic.py
@@ -0,0 +1,17 @@
+# x = print
+# x("hello")
+
+class Classic:
+        
+    def __init__(self):
+        print("i'm printed by init without calling ")
+    def sam(self):
+        print("i'm second")
+
+    def sum(self):
+        print("i'm third")   
+
+s1 = Classic()
+s1.sam()
+s1.sum()
+
